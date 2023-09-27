48d3557d95846a7345ac11c9d83b7398419a773d
mufrid <mufridk@gmail.com>
2023-09-27 13:14:11 +0200

    Added Group and Appointment for GroupPlanning model reflecting that GroupPlanning is looking ahead in time, while GroupCheckin looks back in time

---
+++ GroupTreatment/GroupPlanning/GroupPlanning.Domain/Appointments/Appointments.cs
@@ -0,0 +1,12 @@
+namespace GroupPlanning.Domain;
+
+public class Appointments
+{
+    public Collection<Appointment> PlannedAppointments;
+
+    public Collection<Location> PossibleLocations;
+
+    public Collection<LocationsForAppointments> LocationsForAppointments;
+
+    // some behavior
+}
---
+++ GroupTreatment/GroupPlanning/GroupPlanning.Domain/Group.cs
@@ -0,0 +1,11 @@
+﻿namespace GroupPlanning.Domain;
+
+public class Group
+{
+    public GroupId GroupId;
+    public Doctors SpecialistsInvolvedInGroupTherapy;
+    public Patients PatientsInGroup;
+    public Appointments GroupAppointments;
+
+    // some behavior
+}
